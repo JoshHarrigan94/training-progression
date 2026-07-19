462c457db3424300c08bb4e05e8d8d3cf8ddcd65
JoshHarrigan94 <145808535+JoshHarrigan94@users.noreply.github.com>
2026-07-19 21:53:54 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import { AppProvider, useAppState } from './state/AppContext';
 import { AppShell } from './components/AppShell';
 import { DashboardPage } from './pages/DashboardPage';
-import { ProgrammePage } from './pages/ProgrammePage';
-import { SessionPage } from './pages/SessionPage';
-import { ProgressPage } from './pages/ProgressPage';
 import { ExerciseLibraryPage } from './pages/ExerciseLibraryPage';
+import { ProgrammePage } from './pages/ProgrammePage';
+import { ProgressPage } from './pages/ProgressPage';
+import { SessionPage } from './pages/SessionPage';
 import { SettingsPage } from './pages/SettingsPage';
+import {
+  AppProvider,
+  useAppState,
+} from './state/AppContext';
 
 function ActivePage() {
   const { state } = useAppState();
+
   switch (state.activePage) {
-    case 'programme': return <ProgrammePage />;
-    case 'session': return <SessionPage />;
-    case 'progress': return <ProgressPage />;
-    case 'library': return <ExerciseLibraryPage />;
-    case 'settings': return <SettingsPage />;
-    default: return <DashboardPage />;
+    case 'programme':
+      return <ProgrammePage />;
+
+    case 'session':
+      return <SessionPage />;
+
+    case 'progress':
+      return <ProgressPage />;
+
+    case 'exercise-library':
+      return <ExerciseLibraryPage />;
+
+    case 'settings':
+      return <SettingsPage />;
+
+    case 'dashboard':
+    default:
+      return <DashboardPage />;
   }
 }
 
 export default function App() {
-  return <AppProvider><AppShell><ActivePage /></AppShell></AppProvider>;
+  return (
+    <AppProvider>
+      <AppShell>
+        <ActivePage />
+      </AppShell>
+    </AppProvider>
+  );
 }
